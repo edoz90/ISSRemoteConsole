f5754c5b243944d6a01b1ab52da292f14eb0d9da
Edoardo Rosa <edoardo.rosa90@gmail.com>
2015-01-22 19:46:08 +0100

    new socket management and new buttons

--- www/js/robotWebUI.js
+++ www/js/robotWebUI.js
@@ -1,10 +1,57 @@
-function sendWs(what) {
-	var loc = document.getElementById("location").value;
-	if (loc === "") {
-		loc = "192.168.43.54:8080";
+var speed = 0;
+var speedRobot = "Low";
+var loc = document.getElementById("location").value;
+
+function setSpeed(sp) {
+	switch(sp){
+		case 20:
+			speed = sp;
+			speedRobot = "Low";
+			break;
+		case 50:
+			speed = sp;
+			speedRobot = "Medium";
+			break;
+		case 100:
+			speed = sp;
+			speedRobot = "High";
+			break;
+		default:
+			speed = 20;
+			speedRobot = "Low";
 	}
-	var sock = new WebSocket("ws://"+loc);
-	sock.onopen = function (event) {
-    	sock.send(what);
-    };
+	speed = sp;
+	document.getElementById("speed").value = speed + "%";
+}
+
+function setWs() {
+  	if ("WebSocket" in window) {
+		if (loc === "") {
+			loc = "192.168.43.54:8080";
+		}
+		var sock = new WebSocket("ws://"+loc);
+     	sock.onopen = function() {
+        	// Web Socket is connected, send data using send()
+        	javascript:window.open('control.html','_self');
+     	};
+     	sock.onmessage = function (evt)	{
+        	var received_msg = evt.data;
+     	};
+     	sock.onclose = function() {
+        	alert("Connection Closed");
+     	};
+     	sock.onerror = function(error) {
+     		alert("Connection Error");
+     	}
+	} else {
+		alert("Your does not support WebSocket");
+		navigator.app.exitApp();
+	}
+}
+
+function sendWs(what) {
+	if (what.indexOf("-") >= 0) {
+		what += speedRobot;
+	}
+	sock.send(what);
 }
