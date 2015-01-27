ac0256624ee5e3d3e604af7a6da73dc6eddc22e1
Edoardo Rosa <edoz90@users.noreply.github.com>
2015-01-27 17:23:23 +0100

    Delete CordovaApp-debug-unaligned.apk.d

--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/edoardo/Desktop/RemoteConsole/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/home/edoardo/Desktop/RemoteConsole/platforms/android/ant-build/CordovaApp.ap_ \
-/home/edoardo/Desktop/RemoteConsole/platforms/android/ant-build/classes.dex \
-/home/edoardo/Desktop/RemoteConsole/platforms/android/CordovaLib/ant-build/classes.jar \
