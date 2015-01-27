aaa3cfd5a3033c5354458177d08b59462e388623
Edoardo Rosa <edoz90@users.noreply.github.com>
2015-01-27 17:24:06 +0100

    Delete RemoteConsole-debug-unaligned.apk.d

--- platforms/android/ant-build/RemoteConsole-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/edoardo/Desktop/RemoteConsole/platforms/android/ant-build/RemoteConsole-debug-unaligned.apk : \
-/home/edoardo/Desktop/RemoteConsole/platforms/android/ant-build/RemoteConsole.ap_ \
-/home/edoardo/Desktop/RemoteConsole/platforms/android/ant-build/classes.dex \
-/home/edoardo/Desktop/RemoteConsole/platforms/android/CordovaLib/ant-build/classes.jar \
